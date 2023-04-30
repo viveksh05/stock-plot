c7ce083fc1f3b92ffec0085528cf81ab975cff1b
viveksh05 <viveksh05@outlook.com>
2023-04-30 14:12:59 -0400

    Adding unit tests and version

---
+++ test_stockplot.py
@@ -0,0 +1,10 @@
+import unittest
+from stockplot import stock_info
+
+class StockPlotTest(unittest.TestCase):
+    #initial test
+    def test_stock_info(self):
+        pass
+
+if __name__=="__main__":
+    unittest.main()
